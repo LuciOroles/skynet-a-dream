a44b10d7bc955a9e248d82d9c9f32ebd41e79481
LuciOroles <stoica.florin.lucian@gmail.com>
2021-04-19 22:53:33 +0300

    add Dots drawer

--- src/components/GraphGenerator.tsx
+++ src/components/GraphGenerator.tsx
@@ -40,38 +40,63 @@ const useSVGContext = (canvasRef: RefObject<HTMLDivElement>) => {
   return drawCtx;
 };
 
-type Dot = {
+type DotConfig = {
   radius: number;
   color: string;
 };
+interface AddDot {
+  (d: Coords): void;
+}
 
-const useSvgDotsOnClick = (drawCtx: CanvasRenderingContext2D, dot: Dot) => {
+const useSvgDotsOnClick = (
+  drawCtx: CanvasRenderingContext2D,
+  dot: DotConfig,
+  onAddDot: AddDot
+) => {
   const [svg, setSvg] = useState<SVGElement>();
+  const [listener, setListener] = useState<number>(0);
+
   useEffect(() => {
     const svgS = document.querySelector('#canvas>svg') as SVGElement;
     if (svgS && !svg) setSvg(svgS);
-    if (svg) {
+    if (svg && listener === 0) {
+      setListener(listener + 1);
       svg.addEventListener('click', (e: MouseEvent) => {
         const target = e.target as SVGElement;
         var rect = target.getBoundingClientRect();
         var x = e.clientX - rect.left;
         var y = e.clientY - rect.top;
+        const coords: Coords = {
+          x,
+          y,
+        };
         createCircle(drawCtx, {
           ...dot,
           startPos: {
-            x,
-            y,
+            ...coords,
           },
         });
+        onAddDot(coords);
       });
     }
-  }, [drawCtx, svg, dot]);
+  }, [drawCtx, svg, dot, onAddDot, listener]);
 };
 
 export default function GraphGenerator(): ReactElement {
   const canvasRef = createRef<HTMLDivElement>();
   const drawCtx = useSVGContext(canvasRef);
-  useSvgDotsOnClick(drawCtx, { radius: 7, color: '#ae2aea' });
+  const [dotCollection, setDotCollection] = useState<Coords[]>([]);
+  const handleAddCoords = (c: Coords) => {
+    setDotCollection((d) => {
+      return [...d, c];
+    });
+  };
+  useSvgDotsOnClick(drawCtx, { radius: 7, color: '#542aea' }, handleAddCoords);
 
-  return <div id="canvas" ref={canvasRef} />;
+  return (
+    <div>
+      <div id="canvas" ref={canvasRef} />
+      <span>{JSON.stringify(dotCollection)}</span>
+    </div>
+  );
 }
