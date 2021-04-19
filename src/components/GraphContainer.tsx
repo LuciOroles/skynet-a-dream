4750def12515b2b1fecbf625883985d351c16911
LuciOroles <stoica.florin.lucian@gmail.com>
2021-04-19 21:51:08 +0300

    add dots drawer

--- src/components/GraphContainer.tsx
+++ src/components/GraphContainer.tsx
@@ -1,21 +1,11 @@
 import React, { ReactElement } from 'react';
-import { Draw } from '../context/useCanvas';
+
 import GraphGenerator from './GraphGenerator';
 
 export default function GraphContainer(): ReactElement {
-  const draw: Draw = (ctx, frameCount) => {
-    if (ctx) {
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.fillStyle = '#000000';
-      ctx.beginPath();
-      ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
-      ctx.fill();
-    }
-  };
-
   return (
     <div>
-      <GraphGenerator draw={draw} width={500} height={500} />
+      <GraphGenerator />
     </div>
   );
 }
--- src/components/GraphGenerator.tsx
+++ src/components/GraphGenerator.tsx
@@ -1,30 +1,77 @@
-import React, { MouseEventHandler, ReactElement } from 'react';
-import useCanvas, { Draw } from '../context/useCanvas';
+import React, {
+  ReactElement,
+  createRef,
+  useEffect,
+  useState,
+  RefObject,
+} from 'react';
+import { SVG } from '@svgdotjs/svg.js';
 
-interface Props {
-  draw: Draw;
-  width: number;
-  height: number;
+export type Coords = {
+  x: number;
+  y: number;
+};
+
+type CircleConfig = {
+  radius: number;
+  color: string;
+  startPos: Coords;
+};
+
+function createCircle(draw: any, circleConfig: CircleConfig) {
+  const { startPos } = circleConfig;
+
+  var circle = draw
+    .circle(circleConfig.radius * 2)
+    .attr({ fill: circleConfig.color });
+  circle.move(startPos.x, startPos.y);
+
+  return circle;
 }
 
-export default function GraphGenerator({
-  draw,
-  width,
-  height,
-  ...rest
-}: Props): ReactElement {
-  const canvasRef = useCanvas(draw);
-  const handlClick: MouseEventHandler<HTMLCanvasElement> = (e) => {
-    console.log(e);
-  };
-  return (
-    <canvas
-      className="canvas"
-      ref={canvasRef}
-      width={width}
-      height={height}
-      onClick={handlClick}
-      {...rest}
-    />
-  );
+const useSVGContext = (canvasRef: RefObject<HTMLDivElement>) => {
+  const [drawCtx, setDrawContext] = useState<any>(null);
+  useEffect(() => {
+    if (canvasRef.current && !drawCtx) {
+      setDrawContext(SVG().addTo('#canvas').size(600, 600));
+    }
+  }, [canvasRef, drawCtx]);
+
+  return drawCtx;
+};
+
+type Dot = {
+  radius: number;
+  color: string;
+};
+
+const useSvgDotsOnClick = (drawCtx: CanvasRenderingContext2D, dot: Dot) => {
+  const [svg, setSvg] = useState<SVGElement>();
+  useEffect(() => {
+    const svgS = document.querySelector('#canvas>svg') as SVGElement;
+    if (svgS && !svg) setSvg(svgS);
+    if (svg) {
+      svg.addEventListener('click', (e: MouseEvent) => {
+        const target = e.target as SVGElement;
+        var rect = target.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        createCircle(drawCtx, {
+          ...dot,
+          startPos: {
+            x,
+            y,
+          },
+        });
+      });
+    }
+  }, [drawCtx, svg, dot]);
+};
+
+export default function GraphGenerator(): ReactElement {
+  const canvasRef = createRef<HTMLDivElement>();
+  const drawCtx = useSVGContext(canvasRef);
+  useSvgDotsOnClick(drawCtx, { radius: 7, color: '#ae2aea' });
+
+  return <div id="canvas" ref={canvasRef} />;
 }
