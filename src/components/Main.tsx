398d4b6803bd0c3e454c60fb2fc9f132f840c944
LuciOroles <stoica.florin.lucian@gmail.com>
2021-04-30 11:56:02 +0300

    add loading Ui

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,7 @@ import React, {
   useState,
 } from 'react';
 import { Container, Form, Grid, Button } from 'semantic-ui-react';
+import { Dimmer, Loader, Segment } from 'semantic-ui-react';
 import useCreateGame from '../context/useCreateGame';
 import useGraphData, { Dot, Edge } from '../context/useGraphData';
 import GraphContainer from './GraphContainer';
@@ -40,14 +41,23 @@ export default function Main(): ReactElement {
   const validInput = userId && gameId && role;
 
   const handleStartGame = async () => {
-    if (userId && gameId && ['build', 'connect'].indexOf(role) > -1) {
-      const r = await createGame({
-        userId,
-        gameId,
-        role: role as Roles,
-      });
-      console.log(r);
+    setLoading(true);
+
+    try {
+      if (userId && gameId && ['build', 'connect'].indexOf(role) > -1) {
+        const r = await createGame({
+          userId,
+          gameId,
+          role: role as Roles,
+        });
+        console.log(r);
+      }
+    } catch (error) {
+      console.error('Unable to create game!');
+      alert('Unable to create game!');
     }
+
+    setLoading(false);
   };
 
   const handleGetData = async () => {
@@ -57,76 +67,77 @@ export default function Main(): ReactElement {
     setDots(result.dots);
     setEdges(result.edges);
     setError(result.error);
-
     debugger;
-
     setLoading(false);
   };
-
-  if (loading) {
-    return <div>Loading ... </div>;
-  }
 
   return (
     <Container>
       <Grid columns={2} divided>
         <Grid.Row>
           <Grid.Column>
-            <Form>
-              <Form.Field>
-                <label>Partner public ID:</label>
-                <input
-                  value={userId}
-                  placeholder="public id"
-                  onChange={handleUserChange}
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Game name</label>
-                <input
-                  value={gameId}
-                  placeholder="Game name"
-                  onChange={handleGameIdChange}
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Select your role:</label>
-                <select
-                  value={role}
-                  onChange={(e: SyntheticEvent) => {
-                    const t = e.target as HTMLSelectElement;
+            <Dimmer.Dimmable as={Segment} dimmed={loading}>
+              <Form>
+                <Form.Field>
+                  <label>Partner public ID:</label>
+                  <input
+                    value={userId}
+                    placeholder="public id"
+                    onChange={handleUserChange}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Game name</label>
+                  <input
+                    value={gameId}
+                    placeholder="Game name"
+                    onChange={handleGameIdChange}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Select your role:</label>
+                  <select
+                    value={role}
+                    onChange={(e: SyntheticEvent) => {
+                      const t = e.target as HTMLSelectElement;
 
-                    setRole(t.value);
-                  }}
+                      setRole(t.value);
+                    }}
+                  >
+                    <option value="">Pick one</option>
+                    {Options.map((o) => {
+                      return (
+                        <option value={o.value} key={o.key}>
+                          {o.text}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </Form.Field>
+
+                <Button
+                  type="button"
+                  disabled={!validInput}
+                  primary
+                  onClick={handleStartGame}
                 >
-                  <option value="">Pick one</option>
-                  {Options.map((o) => {
-                    return (
-                      <option value={o.value} key={o.key}>
-                        {o.text}
-                      </option>
-                    );
-                  })}
-                </select>
-              </Form.Field>
-              <Button
-                type="button"
-                disabled={!validInput}
-                primary
-                onClick={handleStartGame}
-              >
-                Start game
-              </Button>
+                  Start game
+                </Button>
 
-              <Button
-                type="button"
-                disabled={!gameId}
-                primary
-                onClick={handleGetData}
-              >
-                Connect to game
-              </Button>
-            </Form>
+                <Button
+                  type="button"
+                  disabled={!gameId}
+                  primary
+                  onClick={handleGetData}
+                >
+                  Connect to game
+                </Button>
+              </Form>
+
+              <Dimmer active={loading}>
+                <Loader />
+              </Dimmer>
+            </Dimmer.Dimmable>
           </Grid.Column>
         </Grid.Row>
       </Grid>
